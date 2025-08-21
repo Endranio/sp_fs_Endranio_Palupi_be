f1d4c15e5dd0a5313477789f91ecd8157378943a
endranio <endranio576@gmail.com>
2025-08-21 10:21:50 +0700

    create search user

--- src/controllers/member-controller.ts
+++ src/controllers/member-controller.ts
@@ -1,48 +1,49 @@
-import { Request,Response,NextFunction } from "express"
-import memberService from "../services/member-service"
+import { Request, Response, NextFunction } from "express";
+import memberService from "../services/member-service";
 
 class MemberController {
+  async getMember(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { projectId } = req.params;
+      const  query  = req.query.search as string;
 
-    async getMember(req:Request, res:Response, next:NextFunction){
-        try {
-            const {projectId} = req.params
-            const member = await memberService.listEligibleUsers(projectId)
-            res.json(member)
-        } catch (error) {
-            next(error)
-        }
+      const member = await memberService.listEligibleUsers(projectId, query);
+      res.json(member);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async getMemberProject(req:Request, res:Response, next:NextFunction){
-        try{
-            const {projectId} = req.params
-            const member = await memberService.getMember(projectId)
-            res.json(member)
-        }catch(error){
-            next(error)
-        }
+  async getMemberProject(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { projectId } = req.params;
+      const member = await memberService.getMember(projectId);
+      res.json(member);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async addMember(req:Request, res:Response, next:NextFunction){
-        try{
-            const {projectId} = req.params
-            const {userId} = req.body
-            const member = await memberService.addMember(userId,projectId)
-            res.json({data:member , message:"Member added"})
-        }catch(error){
-            next(error)
-        }
+  async addMember(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { projectId } = req.params;
+      const { userId } = req.body;
+      const member = await memberService.addMember(userId, projectId);
+      res.json({ data: member, message: "Member added" });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async deleteMember(req:Request, res:Response, next:NextFunction){
-        try {
-            const {userId,projectId} = req.params
-            await memberService.deleteMember(userId,projectId)
-            res.json({message:"Deleted"})
-        } catch (error) {
-            next(error)
-        }
+  async deleteMember(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId, projectId } = req.params;
+      await memberService.deleteMember(userId, projectId);
+      res.json({ message: "Deleted" });
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
-export default new MemberController()
+export default new MemberController();
--- src/services/member-service.ts
+++ src/services/member-service.ts
@@ -1,58 +1,65 @@
 import { prisma } from "../libs/prisma";
 
 class MemberService {
+  async listEligibleUsers(projectId: string, query: string) {
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+      select: { ownerId: true, members: { select: { userId: true } } },
+    });
+    if (!project) throw new Error("Project not found");
 
-   
+    const excludedIds = [
+      project.ownerId,
+      ...project.members.map((m) => m.userId),
+    ];
 
-  async listEligibleUsers(projectId: string) {
-  
-  const project = await prisma.project.findUnique({
-    where: { id: projectId },
-    select: { ownerId: true, members: { select: { userId: true } } },
-  });
-  if (!project) throw new Error('Project not found');
+    return prisma.user.findMany({
+      where: {
+        id: { notIn: excludedIds },
+        OR: query
+          ? [
+              { username: { startsWith: query } },
+              { email: { startsWith: query } },
+            ]
+          : undefined,
+      },
+      select: { id: true, username: true, email: true },
+    });
+  }
 
-  const excludedIds = [
-    project.ownerId,
-    ...project.members.map((m) => m.userId),
-  ];
-
-  return prisma.user.findMany({
-    where: { id: { notIn: excludedIds } },
-    select: { id: true, username: true, email: true },
-  });
-}
-
-async getMember(projectId:string){
+  async getMember(projectId: string) {
     return prisma.membership.findMany({
-        where:{projectId},
-        include:{
-          user:{
-            select:{
-              email:true,
-              username:true
-            }
-          }
-        }
-    })
-}
+      where: { projectId },
+      include: {
+        user: {
+          select: {
+            email: true,
+            username: true,
+          },
+        },
+      },
+    });
+  }
 
-async addMember(userId:string,projectId:string){
-  return await prisma.membership.create({
-    data:{
-      userId,projectId
-    }
-  })
-}
+  async addMember(userId: string, projectId: string) {
+    return await prisma.membership.create({
+      data: {
+        userId,
+        projectId,
+      },
+    });
+  }
 
-async deleteMember(userId:string,projectId:string){
+  async deleteMember(userId: string, projectId: string) {
     return prisma.membership.delete({
-        where:{userId_projectId:{
-            userId,projectId
-        }}
-    })
+      where: {
+        userId_projectId: {
+          userId,
+          projectId,
+        },
+      },
+    });
+  }
 }
 
-}
-
-export default new MemberService()
+export default new MemberService();
